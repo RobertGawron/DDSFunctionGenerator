d748e4260b2923c68bdfeecc22f421411f102f82
Robert Gawron <robert_gawron@yahoo.fr>
2020-01-04 15:46:27 +0100

    split cmd from args

--- tests/BL/TestFramework.fs
+++ tests/BL/TestFramework.fs
@@ -1,9 +1,5 @@
 ( I wasn't able to find a Forth test framework, so here is mine. It's a bit primitive but sufficient. )
 
 : ASSERT (  )
-   
-    IF
-    ELSE
-        0 0 !
-    THEN
+    IF ELSE 0 0 ! THEN
 ;
