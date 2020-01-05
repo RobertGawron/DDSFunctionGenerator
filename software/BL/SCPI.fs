39309df76d45ad54aa50d0142e56b54b09b205d5
Robert Gawron <robert_gawron@yahoo.fr>
2020-01-05 14:12:48 +0100

    word names simplification

--- software/BL/SCPI.fs
+++ software/BL/SCPI.fs
@@ -1,82 +1,80 @@
 ( State machine to parse and execute SCPI commands )
 
-CREATE SCPI_COMMAND 16 CHARS ALLOT
+CREATE SCPI_CMD 16 CHARS ALLOT
 CREATE SCPI_TOKEN   16 CHARS ALLOT
 
-: EXECUTE_*CLS_CMD ( ) 
+: EXEC_*CLS_CMD ( ) 
     ." execute *CLS" CR
 ;
 
-: EXECUTE_*ESE_CMD ( ) ;
-: EXECUTE_*ESE?_CMD ( ) ;
-: EXECUTE_*ESR?_CMD ( ) ;
+: EXEC_*ESE_CMD ( ) ;
+: EXEC_*ESE?_CMD ( ) ;
+: EXEC_*ESR?_CMD ( ) ;
 
-: EXECUTE_*IDN?_CMD ( )
-    GET_IDN 
-    SCPI_RESPONSE_SEND 
+: EXEC_*IDN?_CMD ( )
+    GET_DEVICE_IDENTIFICATOR 
+    SEND_SCPI_RESPONSE 
 ;
 
-: EXECUTE_*OPC_CMD ( ) ;
-: EXECUTE_OPC?_CMD ( ) ;
+: EXEC_*OPC_CMD ( ) ;
+: EXEC_OPC?_CMD ( ) ;
 
-: EXECUTE_*RST_CMD ( )
+: EXEC_*RST_CMD ( )
     RESET_DDS 
     RESET_PGA 
     RESET_ATTENUATORS
 ;
 
-: EXECUTE_*SRE_CMD ( ) ;
-: EXECUTE_*SRE?_CMD ( ) ;
-: EXECUTE_*STB?_CMD ( ) ;
-: EXECUTE_*TST?_CMD ( ) ;
-: EXECUTE_*WAI_CMD ( ) ;
+: EXEC_*SRE_CMD ( ) ;
+: EXEC_*SRE?_CMD ( ) ;
+: EXEC_*STB?_CMD ( ) ;
+: EXEC_*TST?_CMD ( ) ;
+: EXEC_*WAI_CMD ( ) ;
 
-: EXECUTE_:SYSTem_CMD ( )
+: EXEC_:SYSTem_CMD ( )
     ." execute :SYSTem" CR
 ;
 
 ( Compare only first n characters in two strings )
 : COMPARE_STRING ( n -- )
-    \ TODO: move logic from IS_IT_COMMAND_TO_EXECUTE? here
+    \ TODO: move logic from IS_IT_CMD_TO_EXEC? here
 ;
 
-: GET_OFFSET_OF_FIRST_ARG_IN_COMMAND (  -- offset )
+: GET_OFFSET_OF_FIRST_ARG_IN_CMD (  -- offset )
     \ return value 
     0
     
-    \ get SCPI command but don't keep the string on stack
-    SCPI_COMMAND COUNT
+    \ load SCPI command but don't keep the string on stack
+    SCPI_CMD COUNT
     SWAP DROP
        
     \ search for first ":" that is not starting SCPI command
     2 SWAP DO
-        SCPI_COMMAND I CHARS + C@
+        SCPI_CMD I CHARS + C@
         ':' = IF DROP I THEN
     -1 +LOOP
 ;
 
-: IS_IT_COMMAND_TO_EXECUTE? ( cmdToCheck -- cmdToCheck status)
-     TRUE
+: IS_IT_CMD_TO_EXEC? ( -- status )
+    \ return value 
+    TRUE
 
-    \ TODO: should it be here?
-    \ SCPI_TOKEN PLACE 
- 
-    GET_OFFSET_OF_FIRST_ARG_IN_COMMAND 1 -
+    GET_OFFSET_OF_FIRST_ARG_IN_CMD 1 -
     DUP 
     -1 = IF SCPI_TOKEN COUNT THEN
-        2 SWAP DO
-            SCPI_COMMAND I CHARS + C@
-            SCPI_TOKEN I CHARS + C@
-            = IF ELSE DROP FALSE THEN
-        -1 +LOOP
+    2 SWAP DO
+        SCPI_CMD I CHARS + C@
+        SCPI_TOKEN I CHARS + C@
+        = IF ELSE DROP FALSE THEN
+    -1 +LOOP
 ;
 
 ( A command can only start from : or * character )
-: STATE_IS_COMMAND_LEXICALLY_OK? ( -- status )
+: STATE_IS_CMD_LEXICALLY_OK? ( -- status )
     \ return value
     FALSE
 
-    SCPI_COMMAND 1 CHARS + C@
+    SCPI_CMD 1 CHARS + C@
     DUP ':' = SWAP '*' = OR IF DROP TRUE THEN
 ;
 
@@ -85,27 +83,27 @@ CREATE SCPI_TOKEN   16 CHARS ALLOT
     ." Command is incorrect" .S CR 
 ;
 
-: STATE_EXECUTE_COMMAND ( )
+: STATE_EXEC_CMD ( )
     \ temporary hardcoded return value
     TRUE
     
-    S" *CLS"  SCPI_TOKEN PLACE IS_IT_COMMAND_TO_EXECUTE?
+    S" *CLS"  SCPI_TOKEN PLACE IS_IT_CMD_TO_EXEC?
     IF
-       EXECUTE_*CLS_CMD
+       EXEC_*CLS_CMD
     THEN  
 
-    S" :SYSTem"  SCPI_TOKEN PLACE IS_IT_COMMAND_TO_EXECUTE?
+    S" :SYSTem"  SCPI_TOKEN PLACE IS_IT_CMD_TO_EXEC?
     IF
-       EXECUTE_:SYSTem_CMD
+       EXEC_:SYSTem_CMD
     THEN 
 ;
 
 : SCPI_STATE_MACHINE (  )
-    SCPI_COMMAND PLACE
+    SCPI_CMD PLACE
     
-    STATE_IS_COMMAND_LEXICALLY_OK?
+    STATE_IS_CMD_LEXICALLY_OK?
     IF
-        STATE_EXECUTE_COMMAND
+        STATE_EXEC_CMD
         IF 
         ELSE
             STATE_ERROR
