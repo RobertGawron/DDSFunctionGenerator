d6dfce7971093033030f0bef92021348908c131a
Robert Gawron <robert_gawron@yahoo.fr>
2020-10-19 11:29:26 +0200

    add forth interpreter on nucleo based build

--- Software/Common/BusinessLogic/simulation.cpp
+++ Software/Common/BusinessLogic/simulation.cpp
@@ -3,41 +3,15 @@
 #include <stdlib.h>
 #include <stdarg.h>
 
+#include <string>
+#include <fstream>
+#include <streambuf>
+#include <iostream>
+
 extern "C" {
 	#include "zforth.h"
-}
-
-static char buf[32];
-
-int main()
-{
-	/* Initialize zforth */
-
-	zf_init(1);
-	zf_bootstrap();
-	zf_eval(": . 1 sys ;");
 
 
-	/* Main loop: read words and eval */
-
-	uint8_t l = 0;
-
-	for(;;) {
-		int c = getchar();
-		putchar(c);
-		if(c == 10 || c == 13 || c == 32) {
-			zf_result r = zf_eval(buf);
-			if(r != ZF_OK) puts("A");
-			l = 0;
-		} else if(l < sizeof(buf)-1) {
-			buf[l++] = c;
-		}
-
-		buf[l] = '\0';
-	}
-}
-
-extern "C" {
 zf_input_state zf_host_sys(zf_syscall_id id, const char *input)
 {
 	char buf[16];
@@ -67,5 +41,73 @@ zf_input_state zf_host_sys(zf_syscall_id id, const char *input)
                 zf_abort(ZF_ABORT_NOT_A_WORD);
         }
         return v;
+}	
 }
+
+static char buf[32000];
+
+int main()
+{
+	/* Initialize zforth */
+
+	zf_init(1);
+	zf_bootstrap();
+	zf_eval(": . 1 sys ;");
+	uint8_t l = 0;
+
+	std::ifstream SCPI("SCPI.fs");
+	std::string contentSCPI((std::istreambuf_iterator<char>(SCPI)),
+						std::istreambuf_iterator<char>());
+
+	std::ifstream SCPICallbacks("SCPI.fs");
+	std::string contentSCPICallbacks((std::istreambuf_iterator<char>(SCPICallbacks)),
+						std::istreambuf_iterator<char>());
+
+
+	for (auto &letter : contentSCPI) 
+	{
+		std::cout << letter;
+
+		if(letter == 10 || letter == 13 || letter == 32) 
+		{
+			zf_eval(buf);
+			l = 0;
+		}
+		else 
+		{
+			buf[l++] = letter;
+		} 
+	}
+
+	for (auto &letter : contentSCPICallbacks) 
+	{
+		std::cout << letter;
+
+		if(letter == 10 || letter == 13 || letter == 32) 
+		{
+			zf_eval(buf);
+			l = 0;
+		}
+		else 
+		{
+			buf[l++] = letter;
+		} 
+	}
+	/* Main loop: read words and eval */
+
+	
+	for(;;) {
+		int c = getchar();
+		putchar(c);
+		if(c == 10 || c == 13 || c == 32) {
+			zf_result r = zf_eval(buf);
+			if(r != ZF_OK) puts("A");
+			l = 0;
+		} else if(l < sizeof(buf)-1) {
+			buf[l++] = c;
+		}
+
+		buf[l] = '\0';
+	}
 }
+
