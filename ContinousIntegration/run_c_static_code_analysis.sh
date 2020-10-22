103fed9af8d26f26d818fc20a0c03cfb8db034e0
Robert Gawron <robert_gawron@yahoo.fr>
2020-10-22 17:27:50 +0200

    fix for not creating artifact

--- ContinousIntegration/run_c_static_code_analysis.sh
+++ ContinousIntegration/run_c_static_code_analysis.sh
@@ -1,2 +1,2 @@
-cppcheck --enable=all --inline-suppr --force --quiet --error-exitcode=1  ../Software/Common/BusinessLogic/*.c ../Software/Common/BusinessLogic/*.h  
+cppcheck --enable=all --inline-suppr --force --quiet --error-exitcode=1  ../Software/Common/BusinessLogic/*.c ../Software/Common/BusinessLogic/*.h  2>C_Lint_Report.txt   
 
