5031ab0b31b18a046ca706f6a25ff6e668863c8d
Robert Gawron <robert_gawron@yahoo.fr>
2020-12-23 13:47:40 +0100

    Fix script to run component tests.

--- ContinuousIntegration/run_component_tests.sh
+++ ContinuousIntegration/run_component_tests.sh
@@ -1,2 +1,1 @@
-cd ../Software/Common/BusinessLogic && mkdir build && cd build && cmake -DCMAKE_SYSTEM_NAME=Generic -DCMAKE_C_COMPILER=gcc ../ && m
-ake -j12 && cd ../../../../Test/ComponentTest && py.test --html=BusinessLogic_Test_Report.html --self-contained-html test_buisness_logic.py
+cd ../Software/Common/BusinessLogic && mkdir build && cd build && cmake -DCMAKE_SYSTEM_NAME=Generic -DCMAKE_C_COMPILER=gcc ../ && make -j12 && cd ../../../../Test/ComponentTest && py.test --html=BusinessLogic_Test_Report.html --self-contained-html test_buisness_logic.py
