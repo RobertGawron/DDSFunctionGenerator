871939212369dc8ed884c0ad2847d94cb8be0a10
Robert Gawron <robert_gawron@yahoo.fr>
2021-08-07 14:57:22 +0200

    fix #161

--- ContinuousIntegration/run_c_static_code_analysis.sh
+++ ContinuousIntegration/run_c_static_code_analysis.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 cppcheck --enable=all --inline-suppr --force --quiet --error-exitcode=1 --suppress=missingIncludeSystem --check-config  ../Software/Common/BusinessLogic/  -I ../Software/Common/BusinessLogic/ -I ../Software/Common/Libraries/zForth/src/zforth/ -I ../Software/Common/Libraries/u8g2/csrc/ 2>C_Lint_Report.txt
--- ContinuousIntegration/run_component_tests.sh
+++ ContinuousIntegration/run_component_tests.sh
@@ -1,1 +1,3 @@
+#!/bin/bash
+
 cd ../Software/Common/BusinessLogic && mkdir build && cd build && cmake -DCMAKE_SYSTEM_NAME=Generic -DCMAKE_C_COMPILER=gcc ../ && make -j12 && cd ../../../../Test/ComponentTest && py.test --html=BusinessLogic_Test_Report.html --self-contained-html test_buisness_logic.py
